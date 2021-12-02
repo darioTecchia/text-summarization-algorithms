db53af33883e4c9c8907c679b17cd271a63b7fb7
Dario Tecchia <dariotecchia@gmail.com>
2021-12-02 12:58:17 +0100

    update config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SUMMARIES_CHUNK=5
-All_ALGORITHMS = ['bert', 'kl_sum', 'lexrank', 'lsa', 'luhn', 'bart', 't5']
 EXTRACTIVE_ALGORITHMS = ['bert', 'kl_sum', 'lexrank', 'lsa', 'luhn']
 ABSTRACTIVE_ALGORITHMS = ['bart', 't5']
+All_ALGORITHMS = EXTRACTIVE_ALGORITHMS + ABSTRACTIVE_ALGORITHMS
